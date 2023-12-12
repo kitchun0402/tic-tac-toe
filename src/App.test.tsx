8a48ac7592007872a2fc3d372e8cb38cc39fdb15
kitchun0402 <hkc930402@gmail.com>
2023-12-12 18:43:17 +0800

    test: remove unused test

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-/** @todo remove this dummy test later */
-test('renders content', () => {
-  render(<App />)
-  const contentElement = screen.getByText(/CI\/CD/i)
-  expect(contentElement).toBeInTheDocument()
-})
